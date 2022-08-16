41ad2943e410031a87a590d10ef9f90dfe273705
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-08-16 21:03:13 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,33 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sort an unsorted array slowly
- * @array: int param
- * @size: size_t param
- * Return: nothing
+ * bubble_sort - sort an int array with bubble_sort
+ * @array: array of integers of size
+ * @size: the sze of the array
+ *
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	size_t j;
+	/* declarations */
+	int temp;
+	size_t i, j;
 
+	/* check for both array & size */
 	if (!(array && size))
 		return;
-	for (i = 0; i < size; i++)
+
+	/* let's sort this thing */
+	for (i = 0; i < size;)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
-
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
+		i++;
 	}
 }
