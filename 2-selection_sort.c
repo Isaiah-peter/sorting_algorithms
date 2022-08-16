dbae2aad7f70ddf24cd508a5c5c1cb171f161cd9
Isaiah-peter <69847565+Isaiah-peter@users.noreply.github.com>
2022-08-16 21:05:13 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,32 +1,34 @@
 #include "sort.h"
-
 /**
- * selection_sort - sort an array
- * @array: first param
- * @size: second param
- * Return: nothing
+ * selection_sort - sort array of size with selection sorting
+ * @array: int array to sort
+ * @size: how big is the array?
+ *
+ *
  */
-
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, c;
+	/* declarations */
+	size_t a, b, c;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	if (!(array && size))
+		return;
+	/* traverse it, if needed sort it */
+	for (a = 0; a < size - 1; a++)
 	{
-		for (j = i + 1, c = i + 1; j < size; j++)
+		for (b = size - 1, c = a + 1; b > a; b--)
 		{
-			if (array[j] < array[c])
-			{
-				c = j;
-			}
+			if (array[b] < array[c])
+				c = b;
 		}
 
-		if (array[i] > array[c])
+		if (array[a] > array[c])
 		{
-			temp = array[i];
-			array[i] = array[c];
+			temp = array[a];
+			array[a] = array[c];
 			array[c] = temp;
+			/* print every swap */
 			print_array(array, size);
 		}
 	}
